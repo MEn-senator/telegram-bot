515b3d78527bec1b0ff51eaacc0e85bc2ea5df56
ME.n_captain <127752294+MEn-senator@users.noreply.github.com>
2026-04-28 22:05:06 +0330

    Refactor bot.py to improve token handling

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
+BOT_TOKEN = "8632372730:AAEIax1eUT0SY7ddFg2Q4u3qceAXLKqiVh0"
 import os
 BOT_TOKEN = os.getenv("8632372730:AAEIax1eUT0SY7ddFg2Q4u3qceAXLKqiVh0")
 
